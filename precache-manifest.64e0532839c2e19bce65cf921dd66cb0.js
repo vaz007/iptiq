3695a9cfcd46322a17e039d646ae0aea1a2c6349
vaz007 <ankur@adaptideations.com>
2020-09-28 12:04:59 +1000

    Updates

---
+++ precache-manifest.64e0532839c2e19bce65cf921dd66cb0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88644eb33c99d866fc46f0bdbda1bdbe",
+    "url": "/iptiq/index.html"
+  },
+  {
+    "revision": "3f016bffa1f8fd82c06f",
+    "url": "/iptiq/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "96abbca6476462721a24",
+    "url": "/iptiq/static/js/2.87655a21.chunk.js"
+  },
+  {
+    "revision": "8b5507af7aab4f2bcd3bded2a97dafbd",
+    "url": "/iptiq/static/js/2.87655a21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f016bffa1f8fd82c06f",
+    "url": "/iptiq/static/js/main.50aefbd7.chunk.js"
+  },
+  {
+    "revision": "ea002c6cd61081c33c76",
+    "url": "/iptiq/static/js/runtime-main.7e2c696e.js"
+  }
+]);
